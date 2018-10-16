d60e7c10168b3bab745248e484b22fb3e222d17e
Colin Jaffe <colin.jaffe@gmail.com>
2018-10-16 00:34:02 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,10 +2,15 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 const index = 0;
 
+function init() {
+  const body = document.body;
+}
 
+function keydownHandler() {
+  
+}
 
-
-
+init()
 
 
 
